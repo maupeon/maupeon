0a782bbf42ae9eab7369444a35f06f1bf2910ba1
maupeon <maupeon@gmail.com>
2023-07-27 19:02:01 -0600

    Added Religion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -121,11 +121,13 @@ function MobileNavigation(props) {
             </div>
             <nav className="mt-6">
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
-                <MobileNavItem href="/about">About</MobileNavItem>
+                <MobileNavItem href="/religion">Religión</MobileNavItem>
+
+                {/* <MobileNavItem href="/about">About</MobileNavItem>
                 <MobileNavItem href="/articles">Articles</MobileNavItem>
                 <MobileNavItem href="/projects">Projects</MobileNavItem>
                 <MobileNavItem href="/speaking">Speaking</MobileNavItem>
-                <MobileNavItem href="/uses">Uses</MobileNavItem>
+                <MobileNavItem href="/uses">Uses</MobileNavItem> */}
               </ul>
             </nav>
           </Popover.Panel>
@@ -162,11 +164,13 @@ function DesktopNavigation(props) {
   return (
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-        <NavItem href="/about">About</NavItem>
+        <NavItem href="/religion">Religón</NavItem>
+
+        {/* <NavItem href="/about">About</NavItem>
         <NavItem href="/articles">Articles</NavItem>
         <NavItem href="/projects">Projects</NavItem>
         <NavItem href="/speaking">Speaking</NavItem>
-        <NavItem href="/uses">Uses</NavItem>
+        <NavItem href="/uses">Uses</NavItem> */}
       </ul>
     </nav>
   )
@@ -408,10 +412,10 @@ export function Header() {
                   </AvatarContainer>
                 )}
               </div>
-              {/* <div className="flex flex-1 justify-end md:justify-center">
+              <div className="flex flex-1 justify-end md:justify-center">
                 <MobileNavigation className="pointer-events-auto md:hidden" />
                 <DesktopNavigation className="pointer-events-auto hidden md:block" />
-              </div> */}
+              </div>
               <div className="flex justify-end md:flex-1">
                 <div className="pointer-events-auto">
                   <ModeToggle />
---
+++ src/pages/religion.jsx
@@ -0,0 +1,53 @@
+import { SimpleLayout } from '@/components/SimpleLayout'
+import Head from 'next/head'
+import React from 'react'
+
+export default function evangelioDeHoy({ date, first_reading, gospel, psalm }) {
+  console.log({ date, first_reading, gospel, psalm })
+  return (
+    <>
+      <Head>
+        <title>Evangelio de hoy</title>
+        <meta name="description" content="Evangelio de hoy" />
+      </Head>
+      <SimpleLayout title="Evangelio de hoy" intro="">
+        <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+          <h1>Lectura del día: {date}</h1>
+
+          <h2>{first_reading.title}</h2>
+          <p>{first_reading.reference_displayed}</p>
+          <p>{first_reading.text.replace(/\[\[.*?\]\]/g, '')}</p>
+
+          <h2>{gospel.title}</h2>
+          <p>{gospel.reference_displayed}</p>
+          <p>{gospel.text.replace(/\[\[.*?\]\]/g, '')}</p>
+
+          <h2>{psalm.title}</h2>
+          <p>{psalm.text.replace(/\[\[.*?\]\]/g, '')}</p>
+        </div>
+      </SimpleLayout>
+    </>
+  )
+}
+
+export async function getStaticProps(context) {
+  // Crear un objeto Date para la fecha de hoy
+  const today = new Date()
+
+  // Ajustar a la zona horaria local
+  today.setMinutes(today.getMinutes() - today.getTimezoneOffset())
+
+  // Formatear la fecha en el formato 'yyyy-mm-dd'
+  const date = today.toISOString().split('T')[0]
+
+  // Construir la URL
+  const url = `https://evangelium.manuelsanchez.dev/api/es/days/${date}`
+
+  // Fetch the data
+  const response = await fetch(url)
+  const data = await response.json()
+
+  return {
+    props: data, // se pasarán al componente de la página como props
+  }
+}
